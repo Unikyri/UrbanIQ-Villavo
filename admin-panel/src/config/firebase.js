0986079e622be270c19384e585eac7187b6c32f2
McArenas8 <89279901+McArenas8@users.noreply.github.com>
2025-09-20 01:07:34 -0500

    Parche 1: BD en firebase corregido

--- admin-panel/src/config/firebase.js
+++ admin-panel/src/config/firebase.js
@@ -3,14 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// CONFIGURACIÓN TEMPORAL - REEMPLAZA CON LA TUYA
+// Configuración directa (temporal para desarrollo)
 const firebaseConfig = {
-  apiKey: "demo-key",
-  authDomain: "demo.firebaseapp.com",
-  projectId: "demo-project",
-  storageBucket: "demo.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "demo-app-id"
+  apiKey: "AIzaSyDPnz38MkxkeExovNAL-Eg2FyE1_PRfmqw",
+  authDomain: "villavo-conecta.firebaseapp.com",
+  projectId: "villavo-conecta",
+  storageBucket: "villavo-conecta.firebasestorage.app", 
+  messagingSenderId: "1099063898387",
+  appId: "1:1099063898387:web:ca93b6263ee44997efeae6",
+  measurementId:"G-0EJ6LM5YKC"
 };
 
 const app = initializeApp(firebaseConfig);
